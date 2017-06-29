6e9c3dd39bbb1207becb34d56c1f054af864e634
vuongtrieulk <vuongtrieulk@gmail.com>
2017-06-29 16:33:22 +0700

    testing events#show

--- spec/controllers/events_controller_spec.rb
+++ spec/controllers/events_controller_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe EventsController, type: :controller do
 		expect(response).to be_success
 	end
 
+	it "gets show" do
+		a = Event.create!(name: "test", starts_at: 2.days.ago, ends_at: 1.day.from_now, extended_html_description: "a past event",
+		                venue: Venue.new, category: Category.new)
+
+		get :show, id: a.id
+		expect(response).to render_template(:show)
+	end
+
 end
