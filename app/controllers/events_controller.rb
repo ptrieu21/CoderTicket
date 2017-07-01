0a528af8dad394740ab89b6a2d10b6bb5e1732ab
vuongtrieulk <vuongtrieulk@gmail.com>
2017-07-01 20:14:42 +0700

    working on Events#new

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+	before_action :authenticate_user!, except: [:index, :show]
   def index
     if params[:search].present?
     	@events = Event.search(params[:search]).upcoming
@@ -8,7 +9,38 @@ class EventsController < ApplicationController
 
   end
 
+  def my_event
+  	@events = current_user.events
+  end
+
   def show
     @event = Event.find(params[:id])
   end
+
+  def new
+  	@event = Event.new
+  end
+
+  def create
+  	@event = current_user.events.build(event_params)
+  	if @event.save
+  		flash[:success] = "Event has succssfully created!" 
+  	else
+  		render 'new'
+  	end
+  end
+
+
+  private
+  	def event_params
+  		params.require(:event).permit(
+  																	:starts_at, 
+  																	:ends_at, 
+  																	:venue_id, 
+  																	:hero_image_url, 
+  																	:extended_html_description, 
+  																	:category_id, 
+  																	:name
+  																	)
+  	end
 end
