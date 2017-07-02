27efd555854285147492d1ecefd27603c60b0e5c
vuongtrieulk <vuongtrieulk@gmail.com>
2017-07-02 21:27:32 +0700

    Mail on production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,23 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Added per active admin install instructions
+  config.action_mailer.default_url_options = { :host => 'https://trieu-coderticket.herokuapp.com' }
+
+
+  #These settings are for the sending out email for active admin and consequently the   devise mailer
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.raise_delivery_errors = true
+  ActionMailer::Base.smtp_settings = 
+  {
+
+    :address            => 'smtp.gmail.com',
+    :port               => 587,
+    :domain             => 'gmail.com', #you can also use google.com
+    :authentication     => :plain,
+    :user_name          => 'trieutestmail@gmail.com',
+    :password           => 'vuongtrieulk123'
+
 end
